bc6b27dcdc53c2a4b4010e206b9800f05c1340d0
Alejandro.Morales <alejandro.morales@syscaf.com.co>
2016-02-03 10:04:15 -0500

    Primer commit se crea la estructura de directorios y algunos archivos de recomendación

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""@package docstring
+@date:   03/02/2016
+@author: halexci@gmail.com
+
+Documentation for this module.
+
+More details.
+
+para mas información sobre como documentar modulos en python
+con doxygen, por favor revisar el siguiente link:
+http://www.stack.nl/~dimitri/doxygen/manual/docblocks.html#pythonblocks
+"""
+
+print("hola mundo! primer commit")
